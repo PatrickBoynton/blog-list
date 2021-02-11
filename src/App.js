ffeb608bb5a9ece395a9b66ec040db55b729c9b7
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 23:21:06 -0500

    Added a basic look.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,41 @@ import { Component } from 'react';
 import './App.css';
 
 function FullView() {
-  return <p>Full View</p>
+  return (
+    <section>
+      <h1>Fun Blog Post 1</h1>
+      <p>This is a post about some fun things that I did lastd week!
+        What did I do? Well Last week I programmed, then I went on vacation.
+      </p>
+    </section>
+  )
 }
 
 class BlogList extends Component {
   render() {
-    return <p>Blog List</p>
+    return (
+      <aside>
+        <ul>
+        <li>
+          <h1>Fun blog post 1</h1>  
+        </li>
+        <li>
+          <h1>Fun blog post 2</h1>
+          </li>
+        <li>
+          <h1>Fun blog post 3</h1>
+        </li>
+      </ul>
+      </aside>
+    )
   }
 }
 
 class App extends Component {
   render()  {
     return (<div className="App">
-      <BlogList />
       <FullView />
+      <BlogList />
     </div>)
   }
 }
